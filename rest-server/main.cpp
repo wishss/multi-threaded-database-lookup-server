0714ebf3d5c127b82d8bfbf05dcf813e196c0545
wishss <wishkang21@gmail.com>
2024-07-28 12:36:09 +0900

    main.cpp update

--- rest-server/main.cpp
+++ rest-server/main.cpp
@@ -1,10 +1,24 @@
 #include <iostream>
+#include <thread>
+#include "SystemStateMgmt.h"
 #include "ListenerMgmt.h"
 
 int main()
 {
-	ListenerMgmt server("http://localhost:8080/api");
-	server.MgmtStart();
+    // 시스템 상태 관리 시작을 위한 스레드
+    std::thread systemThread([]() {
+        SystemStateMgmt system_state;
+        system_state.startMgmt();
+        });
+
+    // Listener 관리 시작을 위한 스레드
+    std::thread listenerThread([]() {
+        ListenerMgmt listener("http://localhost:8080/api");
+        listener.startMgmt();
+        });
+
+    systemThread.join();
+    listenerThread.join();
 
 	return 0;
 }
